f11fdeae7d78cd0348cf90b0e1c5353b15a4a9fb
daniel <s102590041@gmail.com>
2018-07-30 16:28:35 +0800

    fix verify app

--- faceAPI_verify/verifyApp.py
+++ faceAPI_verify/verifyApp.py
@@ -66,52 +66,5 @@ class verifyApp:
 
             return
 
-    #
-    # if (len(face_info) == 0):
-    #     self.updateLabelText(self.info_labelText, "no face detected")
-    # else:
-    #     self.updateLabelText(self.info_labelText, "face detected")
-    #
-    # new_frame = self.drawRectangle(filename, face_info)
-    #
-    # self.photo = PIL.ImageTk.PhotoImage(image=new_frame)
-    # self.canvas.create_image(0, 0, image=self.photo, anchor=tkinter.NW)
-
     def updateLabelText(self, label_txt, txt):
         label_txt.set(txt)
-
-    def drawRectangle(self, path, parsed):
-        newimg = PIL.Image.open(path)
-        draw = PIL.ImageDraw.Draw(newimg)
-        # 判斷其大小
-        size = len(str(newimg.size[0]))
-        # 根據大小分配字體大小和字的位置
-        if size >= 4:
-            fs = 50
-            ps = 130
-        else:
-            fs = 20
-            ps = 40
-
-        # 圖片的字體和顏色
-        font = PIL.ImageFont.truetype("./font/FreeMono.ttf", fs)
-        draw.ink = 255 + 0 * 256 + 0 * 256 * 256
-
-        # 給每個識別出的人臉畫框、並標識年齡
-        for a in parsed:
-            b = a[u'faceRectangle']
-            c = self.getRectangle(b)
-            draw.rectangle(c, outline='red')
-            temp_text = "Age=" + str(a[u'faceAttributes'][u'age']) + "\n" + "Gender=" + a[u'faceAttributes'][u'gender']
-            draw.text([c[0][0], c[0][1] - ps], temp_text,
-                      font=font)
-        # newimg.show()
-
-        return newimg
-
-    def getRectangle(self, mydata):
-        left = mydata[u'left']
-        top = mydata[u'top']
-        bottom = left + mydata[u'height']
-        right = top + mydata[u'width']
-        return ((left, top), (bottom, right))
